56791af1f8811eab0f7ee5532189862d6c30c0a7
Graydon Svendson <gsvendso@asu.edu>
2016-03-29 12:01:17 -0700

    Added Weapon Base Class

---
+++ Project/Source/Project/WeaponBase.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project.h"
+#include "WeaponBase.h"
+
+
+// Sets default values
+AWeaponBase::AWeaponBase(const FObjectInitializer& ObjectInitializer)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	collisionComponent = ObjectInitializer.CreateDefaultSubobject<UBoxComponent>(this, TEXT("collisionComponent"));
+	RootComponent = collisionComponent;
+
+	meshComponent = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("meshComponent"));
+	meshComponent->AttachTo(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void AWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeaponBase::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void AWeaponBase::Fire()
+{
+	
+}
